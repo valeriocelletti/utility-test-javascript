719bafaea288c2514f99a956e0bf0ca5730ca8fb
mone <simone.fabiano@gmail.com>
2013-09-02 18:16:28 +0200

    import build files and instructions

---
+++ build/app.build.js
@@ -0,0 +1,38 @@
+({
+    baseUrl: "../source",
+    dir: "../built/",
+    keepBuildDir: false,
+    generateSourceMaps: true,
+    optimize: "closure",
+    closure: {
+        CompilationLevel: 'ADVANCED_OPTIMIZATIONS',
+        loggingLevel: 'SEVERE',
+        avoidGlobals: true
+    },
+    
+    paths: {
+        "weswit/Inheritance": "empty:",
+        "weswit/EventDispatcher": "empty:",
+        "weswit/LoggerManager": "empty:",
+        "Inheritance": "empty:"
+    },
+    
+    modules: [
+      {
+        name: "utility-test",
+        include: ["AbstractTest",
+                  "ASSERT",
+                  "TestRunner"
+              ]
+      }
+    ],
+
+    /*
+    wrap: {
+        start: "(function(o) {var l='weswit/';var define = function(c,a,d){for(var b=0;b<a.length;b++)a[b]=l+a[b];o(l+c,a,d)};",
+        end: "}(define));"
+    },
+    */
+    
+    preserveLicenseComments: false
+})
