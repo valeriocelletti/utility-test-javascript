22e81b4eb0a7c31073afcccaf9c8613e7f95d777
mone <simone.fabiano@gmail.com>
2013-09-03 16:47:34 +0200

    refernce external classes without the weswit/ prefix

--- source/TestRunner.js
+++ source/TestRunner.js
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-define(["weswit/Inheritance","weswit/EventDispatcher","./ASSERT"], 
+define(["Inheritance","EventDispatcher","./ASSERT"], 
     function(Inheritance,EventDispatcher,ASSERT) {
 
   /**
