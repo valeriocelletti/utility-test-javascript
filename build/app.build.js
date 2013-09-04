a3ac459cab3ca961c6298e0f120b148bfe50582b
mone <simone.fabiano@gmail.com>
2013-09-04 12:20:12 +0200

    split r.js configuration and add build to generate a weswit-wrapped version

--- build/app.build.js
+++ build/app.build.js
@@ -1,44 +1,5 @@
 ({
-    baseUrl: "../source",
     dir: "../built/",
-    keepBuildDir: false,
-    generateSourceMaps: true,
-    optimize: "closure",
-    closure: {
-        CompilationLevel: 'ADVANCED_OPTIMIZATIONS',
-        loggingLevel: 'SEVERE',
-        externs: [
-          "externs.js"
-        ],
-        ignoreDefaultExterns: false,
-        avoidGlobals: true
-    },
     
-    paths: {
-        "Inheritance": "empty:",
-        "EventDispatcher": "empty:",
-        "LoggerManager": "empty:",
-        "Inheritance": "empty:"
-    },
-    
-    removeCombined: true,
-    
-    modules: [
-      {
-        name: "utility-test",
-        include: ["AbstractTest",
-                  "ASSERT",
-                  "TestRunner"
-              ]
-      }
-    ],
-
-    /*
-    wrap: {
-        start: "(function(o) {var l='weswit/';var define = function(c,a,d){for(var b=0;b<a.length;b++)a[b]=l+a[b];o(l+c,a,d)};",
-        end: "}(define));"
-    },
-    */
-    
-    preserveLicenseComments: false
+    mainConfigFile: "main.config.js"
 })
---
+++ build/main.config.js
@@ -0,0 +1,38 @@
+require({
+
+    baseUrl: "../source",
+    keepBuildDir: false,
+    generateSourceMaps: true,
+    optimize: "closure",
+    closure: {
+        CompilationLevel: 'ADVANCED_OPTIMIZATIONS',
+        loggingLevel: 'SEVERE',
+        externs: [
+          "externs.js"
+        ],
+        ignoreDefaultExterns: false,
+        avoidGlobals: true
+    },
+    
+    paths: {
+        "Inheritance": "empty:",
+        "EventDispatcher": "empty:",
+        "LoggerManager": "empty:",
+        "Inheritance": "empty:"
+    },
+    
+    removeCombined: true,
+    
+    modules: [
+      {
+        name: "utility-test",
+        include: ["AbstractTest",
+                  "ASSERT",
+                  "TestRunner"
+              ]
+      }
+    ],
+
+    preserveLicenseComments: false
+
+})
