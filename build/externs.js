3a898fbd5394fa576c64f3b3203a43e21a9b42d3
mone <simone.fabiano@gmail.com>
2013-09-04 17:52:51 +0200

    introduce Inheritance externs

--- build/externs.js
+++ build/externs.js
@@ -7,3 +7,8 @@ LoggerManager.getLoggerProxy = function() {};
 
 LoggerProxy = {};
 LoggerProxy.logError = function() {};
+
+Inheritance =  function() {};
+Inheritance._super_ =  {};
+Inheritance._callSuperConstructor =  function() {};
+Inheritance._callSuperMethod =  function() {};
